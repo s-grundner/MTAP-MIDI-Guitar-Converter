8a2db172c959066a46f97c5097fde1e289db883b
Laurenz03 <55248383+Laurenz03@users.noreply.github.com>
2022-12-21 09:43:09 +0100

    python tools

---
+++ firmware/Python_Tools/DataProcessor.py
@@ -0,0 +1,22 @@
+arr=[]
+arr = [0 for i in range(8192)] 
+i = 0
+
+with open('sample-data.txt') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.strip()
+        data = float(line) * 25
+        #print(data)
+        arr[i] = data
+        i += 1
+f.close()
+
+file_to_delete = open("processed-data.txt",'w')
+file_to_delete.close()
+
+with open('processed-data.txt', 'w') as d:
+    for data in arr:  
+        d.write(str(data) + ',')
